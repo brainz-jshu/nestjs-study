cc2f4616202d6754b20b024ea759906b7f0d3e48
joonseokhu <vraimentres@gmail.com>
2026-03-09 19:20:00 +0900

    feat: postgres 기본

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,78 +1,51 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-
-export interface PostModel {
-  id: number;
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-
-let posts: PostModel[] = [
-  {
-    id: 1,
-    author: 'John Doe',
-    title: 'My First Post',
-    content: 'This is the content of my first post.',
-    likeCount: 100,
-    commentCount: 20,
-  },
-  {
-    id: 2,
-    author: 'Jane Smith',
-    title: 'My Second Post',
-    content: 'This is the content of my second post.',
-    likeCount: 150,
-    commentCount: 30,
-  },
-  {
-    id: 3,
-    author: 'Alice Johnson',
-    title: 'My Third Post',
-    content: 'This is the content of my third post.',
-    likeCount: 150,
-    commentCount: 30,
-  },
-];
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { PostsModel } from './entities/posts.entity';
 
 @Injectable()
 export class PostsService {
-  getAllPosts(): PostModel[] {
-    return posts;
+  constructor(
+    @InjectRepository(PostsModel)
+    private readonly postRepository: Repository<PostsModel>,
+  ) {}
+
+  async getAllPosts() {
+    return this.postRepository.find();
   }
 
-  getPostById(id: number): PostModel {
-    const post = posts.find((post) => post.id === id);
+  async getPostById(id: number) {
+    const post = await this.postRepository.findOne({ where: { id } });
     if (!post) {
-      throw new NotFoundException(`Post with id ${id} not found`);
+      throw new NotFoundException();
     }
     return post;
   }
 
-  createPost(author: string, title: string, content: string): PostModel {
-    const post: PostModel = {
-      id: posts[posts.length - 1].id + 1,
+  async createPost(author: string, title: string, content: string) {
+    const post = this.postRepository.create({
       author,
       title,
       content,
       likeCount: 0,
       commentCount: 0,
-    };
-    posts = [...posts, post];
-    return post;
+    });
+    const newPost = await this.postRepository.save(post);
+    return newPost;
   }
 
-  updatePost(
+  async updatePost(
     postId: number,
     author?: string,
     title?: string,
     content?: string,
-  ): PostModel {
-    const post = posts.find((post) => post.id === Number(postId));
+  ) {
+    const post = await this.postRepository.findOne({ where: { id: postId } });
+
     if (!post) {
-      throw new NotFoundException(`Post with id ${postId} not found`);
+      throw new NotFoundException();
     }
+
     if (author) {
       post.author = author;
     }
@@ -82,16 +55,19 @@ export class PostsService {
     if (content) {
       post.content = content;
     }
-    posts = posts.map((prev) => (prev.id === Number(postId) ? post : prev));
-    return post;
+
+    const newPost = await this.postRepository.save(post);
+    return newPost;
   }
 
-  deletePost(postId: number): number {
-    const post = posts.find((post) => post.id === Number(postId));
+  async deletePost(postId: number) {
+    const post = await this.postRepository.findOne({ where: { id: postId } });
+
     if (!post) {
-      throw new NotFoundException(`Post with id ${postId} not found`);
+      throw new NotFoundException();
     }
-    posts = posts.filter((post) => post.id !== Number(postId));
+
+    await this.postRepository.delete(postId);
 
     return postId;
   }
